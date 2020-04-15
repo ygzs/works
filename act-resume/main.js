2511ebf6c4f8abc2a01c7d60f69a94f1adf79237
ygzs <1103449238@qq.com>
2020-04-15 18:50:37 +0800

    改进

--- act-resume/main.js
+++ act-resume/main.js
@@ -1,3 +1,17 @@
+function writeleft(preview,text,fn){
+    let n = 0
+    let timer = setInterval( () => {
+        n += 1
+        words.innerHTML = Prism.highlight( preview + text.substring(0,n), Prism.languages.css, 'css')
+        style.innerHTML = preview + text.substring(0,n)
+        words.scrollTop = words.scrollHeight
+        if (n>=text.length) {
+            window.clearInterval(timer)
+            fn.call()
+        }
+    },10) 
+}
+
 var result = `/*
 
 *面试官你好，我是XXX
@@ -19,8 +33,7 @@ html{
 }
 
 #words{
-    border: 1px solid red;
-    padding: 20px;
+    padding: 40px;
 }
 
 /*我需要一点代码高亮*/
@@ -39,43 +52,74 @@ html{
 /*现在开始介绍一下自己*/
 /*我需要一张白纸*/
 
-*/`
+`
 
-var n = 0
-var timer = setInterval( () => {
-    n += 1
-    words.innerHTML = result.substring(0,n)
-    words.innerHTML = Prism.highlight( words.innerHTML, Prism.languages.css, 'css')
-    style.innerHTML = result.substring(0,n)
-    if (n>=result.length) {
-        window.clearInterval(timer)
-        fn2()
-        fn3(result)
-    }
-},10)
+var result2 = `
+#words{
+    position: fixed;
+    left: 0px;
+    width: 50%;
+    height: 100%;
+}
+#paper{
+    position: fixed;
+    right: 0px;
+    width: 50%;
+    height: 100%;
+    padding: 20px;
+    background-color: darkgrey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+#content{
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    padding-left: 16px;
+}
+`
+var result3 = `
+#   我是zp
+    只见远处有一座迷蒙的巨峰突起，周围还有几十座小石峰。
+    仔细一看，那巨峰像手握金箍棒的孙悟空，那些小峰就像抓耳腮的小猴。
+    瞧瞧，孙悟空正领着它的孩子们向南天门杀去呢。
+    微白的天空下，群山苍黑似铁，庄严、肃穆。
+    红日初升，一座座山峰呈墨蓝色。
+    紧接着，雾霭泛起，乳白的纱把重山间隔起来，
+    只剩下青色的峰尖，真像一幅笔墨清爽、疏密有致的山水画。
+    过了一阵儿，雾又散了，那裸露的岩壁，峭石，被霞光染得赤红，
+    渐渐地又变成古铜色，与绿的树、绿的田互为映衬，显得分外壮美。
+`
 
-function fn2(){
+
+writeleft('', result, ()=>{
+    createPaper( () => {
+        writeleft(result, result2, ()=>{
+            writeright(result3)
+        })   
+    })   
+})
+
+function createPaper(fn){
     var paper = document.createElement('div')
     paper.id = 'paper'
+    var content = document.createElement('pre')
+    content.id = 'content'
+    paper.appendChild(content)
     document.body.appendChild(paper)
+    fn.call()
 }
 
-function fn3(){
-    var result2 = `
-#paper{
-    width: 100px;
-    height: 100px;
-    background: red;
-}
-    `
-    var n = 0
-    var timer = setInterval( () => {
-    n += 1
-    words.innerHTML = result + result2.substring(0,n)
-    words.innerHTML = Prism.highlight( words.innerHTML, Prism.languages.css, 'css')
-    style.innerHTML = result + result2.substring(0,n)
-    if (n>=result.length) {
-        window.clearInterval(timer)
-    }
-    },10)
+function writeright(text,fn){
+    let n = 0
+    let timer = setInterval( () => {
+        n += 1
+        content.innerHTML = result3.substring(0,n)
+        content.scrollTop = content.scrollHeight
+        if (n>=text.length) {
+            window.clearInterval(timer)
+            fn.call()
+        }
+    },10) 
 }
